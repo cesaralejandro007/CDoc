d2f8377776d4716ce23b7310dfc344c20a285c97
cesaralejandro007 <cesaralejandrovidesgonzalez@gmail.com>
2024-08-29 20:35:42 -0400

    bitacora del sistema

--- bin/config/connect/connectDB.php
+++ bin/config/connect/connectDB.php
@@ -29,9 +29,16 @@
 				die();
 			}
 		}
-
+	
 		public function registrar_bitacora($id_usuario,$accion)
 		{
+
+			$motodo_solicitud =	$_SERVER['REQUEST_METHOD'];
+			$ip_cliente =	$_SERVER['REMOTE_ADDR'];
+			$nombre_servidor =	$_SERVER['SERVER_NAME'];
+			$ruta_script =	$_SERVER['SCRIPT_NAME'];
+			$user_agent =	$_SERVER['HTTP_USER_AGENT'];
+
 			try {
 				// Verificar si ya existe un registro en la tabla `historial` para el usuario
 				$result = $this->conex->query("SELECT accion FROM historial WHERE id_usuario = '$id_usuario'");
@@ -40,9 +47,11 @@
 					$row = $result->fetch();
 					$nueva_accion = $accion . " / " . $row['accion'] ;
 					$this->conex->query("UPDATE historial SET accion = '$nueva_accion' WHERE id_usuario = '$id_usuario'");
+					$this->conex->query("INSERT INTO bitacora_sistema(id_usuario,motodo_solicitud,ip_cliente,nombre_servidor,ruta_script,user_agent) VALUES('$id_usuario', '$motodo_solicitud', '$ip_cliente', '$nombre_servidor', '$ruta_script', '$user_agent')");
 				} else {
 					// Si no existe, insertar un nuevo registro en la tabla `historial`
 					$this->conex->query("INSERT INTO historial(id_usuario, accion) VALUES('$id_usuario', '$accion')");
+					$this->conex->query("INSERT INTO bitacora_sistema(id_usuario,motodo_solicitud,ip_cliente,nombre_servidor,ruta_script,user_agent) VALUES('$id_usuario', '$motodo_solicitud', '$ip_cliente', '$nombre_servidor', '$ruta_script', '$user_agent')");
 				}   
 			} catch (Exception $e) {
 				return false;
